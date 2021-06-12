65e3cb098389a9016078ba10aaf34e6006b6ad12
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-06-12 12:32:47 +0530

    add Home Page

--- src/Controller/HomeFormController.java
+++ src/Controller/HomeFormController.java
@@ -1,7 +1,12 @@
 package Controller;
 
+import javafx.scene.layout.AnchorPane;
+
 public class HomeFormController {
 
-
+    public JFXRippler rprAddNewStudent;
+    public AnchorPane pneAddNewStudent;
+    public JFXRippler rprSearchStudents;
+    public AnchorPane pneSearchStudents;
 
 }
