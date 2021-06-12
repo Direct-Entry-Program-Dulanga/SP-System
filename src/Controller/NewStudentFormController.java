c1fcf14584f023a8aa1d3daf780c974898dbe416
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-06-12 19:27:52 +0530

    add Student Form CSS

--- src/Controller/NewStudentFormController.java
+++ src/Controller/NewStudentFormController.java
@@ -1,4 +1,21 @@
 package Controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import util.MaterialUI;
+
 public class NewStudentFormController {
+
+    public TextField txtStudentName;
+    public TextField txtAddress;
+    public TextField txtPhone;
+    public TextField txtEmail;
+    public RadioButton rbnDep;
+    public RadioButton rbnGDse;
+    public JFXButton btnSave;
+
+    public void initialize(){
+        MaterialUI.paintTextFields(txtStudentName, txtAddress, txtEmail, txtPhone);
+    }
 }
