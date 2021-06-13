d551b8c560589ad24c7ba0a81816905fb6e293b0
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-06-13 16:59:19 +0530

    add admin view Student page

--- src/Controller/LoginFormController.java
+++ src/Controller/LoginFormController.java
@@ -17,12 +17,12 @@ public class LoginFormController {
     public TextField txtUserName;
     public Label lblError;
 
-    public JFXRippler rprLoginUser;
+    public JFXRippler rprLogin;
     public AnchorPane pneLoginUser;
 
 
     public void initialize() {
-        rprLoginUser.setControl(pneLoginUser);
+        rprLogin.setControl(pneLoginUser);
         pneLoginUser.setFocusTraversable(true);
     }
 
@@ -53,35 +53,40 @@ public class LoginFormController {
 //    }
 
 
-//    public void pneLoginUser_OnKeyPressed(KeyEvent keyEvent) {
+//    public void pneLoginUser_OnKeyPress(KeyEvent keyEvent) {
 //        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
 //            rprLoginUser.createManualRipple().run();
 //        }
 //    }
 //
-//    public void pneLoginUser_OnKeyReleased(KeyEvent keyEvent) {
+//    public void pneLoginUser_OnKeyRelease(KeyEvent keyEvent) {
 //        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
-////            navigate("Student Payment System", "/View/HomeForm.fxml");
+//           navigate("Student Payment System", "/View/HomeForm.fxml");
 //        }
 //    }
-
-    public void pneLoginUser_OnMouseClicked(MouseEvent mouseEvent) {
-        if(txtUserName.getText().equals("Dulanga") && txtPassword.getText().equals("123")){
-            navigate("Student Payment System", "/View/HomeForm.fxml");
-            lblError.setVisible(false);
-            lblError.setText("Success");
-        }else if(txtUserName.getText().isEmpty() && txtPassword.getText().isEmpty()){
-            lblError.setVisible(true);
-            lblError.setText("Please Enter your UserName & Password...");
-        }else{
-            lblError.setVisible(true);
-            lblError.setText("Enter Correct Username & Password");
-        }
-    }
-
-    private void navigate(String title, String url) {
-        MainFormController ctrl = (MainFormController) pneLoginUser.getScene().getUserData();
-        ctrl.navigate(title, url, AppBarIcon.NAV_ICON_BACK, () ->
-                ctrl.navigate("Student Management System", "/View/LoginForm.fxml", AppBarIcon.NAV_ICON_HOME));
-    }
+//
+//    public void pneLoginUser_OnMouseClick(MouseEvent mouseEvent) {
+//        if(txtUserName.getText().equals("User") && txtPassword.getText().equals("123")){
+//            navigate("Student Payment System", "/View/HomeForm.fxml");
+//            lblError.setVisible(false);
+//            lblError.setText("Success");
+//
+//        }else if(txtUserName.getText().equals("Admin") && txtPassword.getText().equals("123")){
+//            navigate("Student Payment System", "/View/HomeForm.fxml");
+//            lblError.setVisible(false);
+//            lblError.setText("Success");
+//        }else if(txtUserName.getText().isEmpty() && txtPassword.getText().isEmpty()){
+//            lblError.setVisible(true);
+//            lblError.setText("Please Enter your UserName & Password...");
+//        }else{
+//            lblError.setVisible(true);
+//            lblError.setText("Enter Correct Username & Password");
+//        }
+//    }
+//
+//    private void navigate(String title, String url) {
+//        MainFormController ctrl = (MainFormController) pneLoginUser.getScene().getUserData();
+//        ctrl.navigate(title, url, AppBarIcon.NAV_ICON_BACK, () ->
+//                ctrl.navigate("Student Management System", "/View/LoginForm.fxml", AppBarIcon.NAV_ICON_HOME));
+//    }
 }
---
+++ src/Controller/StudentChangeFormController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import util.MaterialUI;
+
+public class StudentChangeFormController {
+
+
+    public TextField txtQuery;
+    public TableView tblAdminStudents;
+
+    public void initialize(){
+        MaterialUI.paintTextFields(txtQuery);
+    }
+    public void tblResults_OnKeyPressed(KeyEvent keyEvent) {
+    }
+}
