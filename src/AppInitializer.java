f832ecc6b0659c480e4c420218ee6f764a66e7e9
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-06-29 02:48:41 +0530

    update appinitializer

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,4 +1,5 @@
 import Controller.MainFormController;
+import javafx.scene.control.Alert;
 import util.AppBarIcon;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -14,10 +15,19 @@ public class AppInitializer extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
     }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+
+        try{
+            spinUpRedisServerInstance();
+        }catch (Exception e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to load data, please contact").show();
+            return;
+        }
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/View/MainForm.fxml"));
         Parent root = fxmlLoader.load();
         Scene mainScene = new Scene(root);
@@ -30,5 +40,16 @@ public class AppInitializer extends Application {
         primaryStage.setTitle("Student Payment System");
         primaryStage.show();
         primaryStage.centerOnScreen();
+    }
+
+    private void spinUpRedisServerInstance() throws Exception {
+        String[] commands = {"redis-server", "redis.conf"};
+        Process redisServer = Runtime.getRuntime().exec(commands);
+
+        int exitCode = redisServer.waitFor();
+
+        if (exitCode != 0) {
+            throw new Exception("Failed to spin up the redis server instance");
+        }
     }
 }
