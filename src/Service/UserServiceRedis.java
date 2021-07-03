77ed5e6538592673461ee163f7885909e4ec7a30
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-07-03 13:46:47 +0530

    User Login commit

---
+++ src/Service/UserServiceRedis.java
@@ -0,0 +1,31 @@
+package Service;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import redis.clients.jedis.Jedis;
+import util.JedisClient;
+
+public class UserServiceRedis {
+
+    private static final String DB_PREFIX = "u#";
+
+    private final Jedis client;
+
+    public UserServiceRedis() {
+        client = JedisClient.getInstance().getClient();
+    }
+
+    public void createUser(){}
+
+    public void updateUser(){};
+
+    public boolean authenticate(String username, String password){
+        if (!client.exists(DB_PREFIX + username)){
+            return false;
+        }
+
+        String originalPwdHash = client.get(DB_PREFIX + username);
+        String pwdHash = DigestUtils.sha256Hex(password);
+
+        return (originalPwdHash.equals(pwdHash));
+    }
+}
